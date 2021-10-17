97333f45d9c0bb3fa4f096ab967e563b610c3d90
Nishat009 <nishatraihana009@gmail.com>
2021-10-18 00:13:47 +0600

    Add basic search functionality in the Grid of movies page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
 import Movies from "../Movies/Movies";
-import './Home.css'
+import { Form, FormControl } from "react-bootstrap";
+import "./Home.css";
 const Home = () => {
   const [movies, setMovies] = useState([]);
   const [pageNumber, setPageNumber] = useState(0);
-
+  const [searchTerm, setSearchTerm] = useState("");
   const moviePerPage = 10;
 
   const pagesVisited = pageNumber * moviePerPage;
 
   const displayMovie = movies
     .slice(pagesVisited, pagesVisited + moviePerPage)
-
+    .filter((movie) => {
+      if (searchTerm === "") {
+        return movie;
+      } else if (movie.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+        return movie;
+      }
+    })
     .map((movie) => (
       <div className=" col-md-6 col-lg-4 col-sm-12">
         {" "}
@@ -20,11 +27,11 @@ const Home = () => {
       </div>
     ));
 
-    const pageCount= Math.ceil(movies.length /moviePerPage)
+  const pageCount = Math.ceil(movies.length / moviePerPage);
 
-    const changePage=({selected})=>{
-        setPageNumber(selected)
-    }
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/4/list/7096014?page=1&api_key=4275cf25831de3b150d6ae572b31a179`
@@ -38,20 +45,33 @@ const Home = () => {
 
   return (
     <div className="container">
-      <div className=" row mb-5">{displayMovie}
-       <ReactPaginate
-      previousLabel={"prev"}
-      nextLabel={"next"}
-      pageCount={pageCount}
-      onPageChange={changePage}
-      containerClassName={"paginationBttns"}
-      previousLinkClassName={"previousBttn"}
-      nextLinkClassName={"nextBttn"}
-      disabledClassName={"paginationDisabled"}
-      activeClassName={"paginationActive"}
-
-      ></ReactPaginate>
-      <p>{moviePerPage} out of {movies.length}</p>
+      <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="search-button "
+          aria-label="Search"
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+      </Form>
+      <div className=" row mb-5">
+        {displayMovie}
+        <ReactPaginate
+          previousLabel={"prev"}
+          nextLabel={"next"}
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={"paginationBttns"}
+          previousLinkClassName={"previousBttn"}
+          nextLinkClassName={"nextBttn"}
+          disabledClassName={"paginationDisabled"}
+          activeClassName={"paginationActive"}
+        ></ReactPaginate>
+        <p>
+          {moviePerPage} out of {movies.length}
+        </p>
       </div>
     </div>
   );
