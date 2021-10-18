8501e2de24b0abf1a333236384eaa4d66c406ef6
Nishat009 <nishatraihana009@gmail.com>
2021-10-18 15:51:55 +0600

    search field that will have server-side searching

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,9 +6,10 @@ import "./Home.css";
 const Home = () => {
   const [movies, setMovies] = useState([]);
   const [pageNumber, setPageNumber] = useState(0);
+  const [searchMovie, setSearchMovie] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
 
-  const moviePerPage = 5;
+  const moviePerPage = 9;
 
   const pagesVisited = pageNumber * moviePerPage;
 
@@ -33,48 +34,87 @@ const Home = () => {
   const changePage = ({ selected }) => {
     setPageNumber(selected);
   };
+
   useEffect(() => {
-    fetch(
+    getMovies(
       `https://api.themoviedb.org/4/list/7096014?page=1&api_key=4275cf25831de3b150d6ae572b31a179`
-    )
+    );
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
         setMovies(data.results);
-        console.log(data);
       });
-  }, []);
+  };
+  const searchAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=4275cf25831de3b150d6ae572b31a179&query=";
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchMovie) {
+      getMovies(searchAPI + searchMovie);
+      setSearchMovie("");
+    }
+  };
+  const handleOnChange = (e) => {
+    setSearchMovie(e.target.value);
+  };
   return (
-    <div className="container">
-      <Form className="d-flex">
-        <FormControl
-          type="search"
-          placeholder="Search"
-          className="search-button "
-          aria-label="Search"
-          onChange={(e) => {
-            setSearchTerm(e.target.value);
-          }}
-        />
-      </Form>
-      <div className=" row mb-5">
-        {displayMovie}
-        <ReactPaginate
-          previousLabel={"prev"}
-          nextLabel={"next"}
-          pageCount={pageCount}
-          onPageChange={changePage}
-          containerClassName={"paginationBttns"}
-          previousLinkClassName={"previousBttn"}
-          nextLinkClassName={"nextBttn"}
-          disabledClassName={"paginationDisabled"}
-          activeClassName={"paginationActive"}
-        ></ReactPaginate>
-        <p>
-          {pagesVisited + moviePerPage} out of {movies.length}
-        </p>
+    <>
+      <div
+        style={{
+          height: "60px",
+          padding: "10px 5px 10px 5px",
+          backgroundColor: "#032541",
+        }}
+        
+      >
+        <Form onSubmit={handleSubmit}>
+          <FormControl
+            type="search"
+            placeholder="server-side searching"
+            className="search-button search justify-content-between  "
+            aria-label="Search"
+            value={searchMovie}
+            onChange={handleOnChange}
+         
+          />
+        </Form>
       </div>
-    </div>
+      <div className="container">
+        <Form>
+          <FormControl
+            type="search"
+            placeholder="Search the Movie"
+            className="search-button text-right"
+            aria-label="Search"
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+          />
+        </Form>
+
+        <div className=" row mb-5">
+          {displayMovie}
+          <ReactPaginate
+            previousLabel={"prev"}
+            nextLabel={"next"}
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={"paginationBttns"}
+            previousLinkClassName={"previousBttn"}
+            nextLinkClassName={"nextBttn"}
+            disabledClassName={"paginationDisabled"}
+            activeClassName={"paginationActive"}
+          ></ReactPaginate>
+          <p>
+            {pagesVisited + moviePerPage} out of {movies.length}
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
 
 const Movies = ({
-  movie,
   title,
   overview,
   vote_average,
   vote_count,
   release_date,
+  
 }) => {
   const [show, setShow] = useState(false);
 
@@ -15,21 +15,24 @@ const Movies = ({
   const handleShow = () => setShow(true);
   return (
     <div>
-      <div onClick={handleShow} class="card card-design shadow-sm mb-2  mt-3">
-        <div className="card-body">
-          <h3 className="card-title p-2">{title}</h3>
-        </div>
+      <div onClick={handleShow} class="">
+        <ul className="" style={{ backgroundColor:"#032541", color:"#FFFFFF" }}>
+          <li  style={{cursor: "pointer"}} className="card-title p-2">{title}</li>
+          
+        </ul>
       </div>
       <Modal
         show={show}
         onHide={handleClose}
         backdrop="static"
         keyboard={false}
+        style={{cursor: "pointer"}}
       >
         <Modal.Header closeButton>
           <Modal.Title>Movie Name: {title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
+      
           <span style={{ fontWeight: "bold" }}>Short Overview of Movie:</span>{" "}
           {overview} <br />
           <span style={{ fontWeight: "bold" }}>Release Date</span>{" "}
